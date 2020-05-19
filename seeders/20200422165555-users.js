8b57d202d893045a6775f65a3aa2d033585d967a
Hongoatomic <m.sol.escobar@gmail.com>
2020-05-19 16:42:29 -0300

    is admin heroku change

--- seeders/20200422165555-users.js
+++ seeders/20200422165555-users.js
@@ -1,25 +1,33 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 /* eslint-disable no-unused-vars */
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.bulkInsert('Users', [{
-    username: 'sescobar',
-    firstname: 'Sol',
-    lastname: 'Escobar',
-    email: 'sescobar@gmail.com',
-    address: 'direccion 202',
-    phone_number: '2223333',
-    password: bcrypt.hashSync('123456', 10),
-    is_admin: true
-  },
-  {
-    username: 'dogglover',
-    firstname: 'Bret',
-    lastname: 'Leanne',
-    email: 'bret@yahoo.com',
-    address: 'Kulas Light Apt. 556',
-    phone_number: '2223333',
-    password: bcrypt.hashSync('17707368031', 10),
-    is_admin: false
-  }], {}),
-  down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Users', null, {}),
-}
+    up: (queryInterface, Sequelize) =>
+        queryInterface.bulkInsert(
+            "Users",
+            [
+                {
+                    username: "sescobar",
+                    firstname: "Sol",
+                    lastname: "Escobar",
+                    email: "sescobar@gmail.com",
+                    address: "direccion 202",
+                    phone_number: "2223333",
+                    password: bcrypt.hashSync("123456", 10),
+                    is_admin: 1,
+                },
+                {
+                    username: "dogglover",
+                    firstname: "Bret",
+                    lastname: "Leanne",
+                    email: "bret@yahoo.com",
+                    address: "Kulas Light Apt. 556",
+                    phone_number: "2223333",
+                    password: bcrypt.hashSync("17707368031", 10),
+                    is_admin: 0,
+                },
+            ],
+            {}
+        ),
+    down: (queryInterface, Sequelize) =>
+        queryInterface.bulkDelete("Users", null, {}),
+};
